14238c6ca96c6a37259ab263e7fb2be522ee176a
saviour <saviourjeremiah2024@gmail.com>
2025-03-20 16:33:55 -0700

    second

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -48,6 +48,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
+
+
 ]
 
 ROOT_URLCONF = 'ecommerce.urls'
