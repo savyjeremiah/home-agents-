db51977bc3285ed05b104752184001b3a15d2e75
saviour <saviourjeremiah2024@gmail.com>
2025-03-23 01:30:54 -0700

    Resolved merge conflicts

--- manage.py
+++ manage.py
@@ -6,7 +6,11 @@ import sys
 
 def main():
     """Run administrative tasks."""
+<<<<<<< HEAD
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
+=======
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'house.settings')
+>>>>>>> 8634f16 (Synced with remote and added local changes)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
